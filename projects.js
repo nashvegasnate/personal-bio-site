43c728f3e449a8e692ca8c610a6815cd9bbf2b4b
Nathan Keith <nashvegasnate@gmail.com>
2021-01-31 22:22:52 -0600

    made corrections in code

--- projects.js
+++ projects.js
@@ -27,8 +27,8 @@ const projects = [
   available: false,
   url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
   githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux",
-},
-];
+}
+]
 
 const printToDom = (divId, textToPrint) => {
   const selectedDiv = document.querySelector(divId);
@@ -38,31 +38,26 @@ const printToDom = (divId, textToPrint) => {
 const createProjectCards = (taco) => {
   let domString = '';
 
-  for (let i = 0; i < taco.length; i++) {
-      domString += `<div class="card" style="width: 18rem;" id${i}> 
-      <img src="url('${taco[i].imageUrl}');" class="card-img-top" alt="blah">
-      <div class="card-body">
-        <p class="card-text">${taco[i].projectName}</p>
-      </div>
-    </div>`
-  }
-
   for (let [i, element] of taco.entries()) {
-    domString += `<div class="card" style="width: 18rem;" id${i}> 
-    <img src="url('${taco[i].imageUrl}');" class="card-img-top" alt="blah">
-    <div class="card-body">
-      <p class="card-text">${taco[i].projectName}</p>
-    </div>
-  </div>`
+    domString += `<div class="card" style="width: 18rem;" id=${i}> 
+                    <img src=${element.screenshot} class="card-img-top" alt="blah">
+                      <div class="card-body">
+                        <p class="card-text">${element.title}</p>
+                        <p class="card-text">${element.description}</p>
+                        <p class="card-text">${element.technologiesUsed}</p>
+                        <p class="card-text">${element.available}</p>
+                        <p class="card-text">${element.url}</p>
+                        <p class="card-text">${element.githubUrl}</p>  
+                      </div>
+                  </div>`;
   }
-
   printToDom('#projects', domString);
 }
 
-
+console.log("HEY");
 
 const init = () => {
   createProjectCards(projects);
 }
 
-init ();
+init();
